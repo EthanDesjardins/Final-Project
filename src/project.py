2adb5a7dbd6a3bb9ad06a40653aec2984691906f
EthanDesjardins <epd210000@utdallas.edu>
2023-11-30 14:54:02 -0600

    started rounds function

--- src/project.py
+++ src/project.py
@@ -7,6 +7,18 @@ def dice():
     
     return roll
 
+def rounds():
+    max_health = 100
+    player_health = 100
+    current_damage = 0
+
+    keep_roll = input("Would you like to keep rolling? yes/no")
+    if keep_roll == "yes":
+        score = dice()
+    
+    if keep_roll == "no":
+        print("Your turn is over")
+
 def main():
 
     while True:
@@ -17,9 +29,13 @@ def main():
             if 1 <= rounds <= 10:
                 break
         else:
-            print("Invalid, please enetr a number between 1 and 4.")
-    roll = dice()
-    print(f"Player 1 your roll is {roll}, do you want to roll again?")
+            print("Invalid, please enetr a number between 1 and 10.")
+    
+    rounds()
+    # roll = dice()
+    # print(f"Player 1 your roll is {roll}, do you want to roll again?")
+    # roll = roll + dice()
+    # print(f"Player 1 your roll is {roll}, do you want to roll again?")
 
 if __name__ == "__main__":
     main()
