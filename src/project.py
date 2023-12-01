243a6918694670c8d41e601f68a1a3859f402a81
EthanDesjardins <epd210000@utdallas.edu>
2023-12-01 01:24:55 -0600

    changed code, can't figure out how to save player values

--- src/project.py
+++ src/project.py
@@ -7,9 +7,6 @@ def dice():
     
     return roll
 
-def rounds():
-   
-
 def main():
 
     while True:
@@ -25,35 +22,49 @@ def main():
 
 
 #
-    player_health = 100
-    # player1_points = 0
-    # player2_points = 0
-    # player1_damage = 0
-    # player2_damage = 0
+    player_health = 20
+    player1_health = player_health
+    player2_health = player_health
+    players = 2
     current_damage = 0
-    player_score = ["player1" , "player2"]
+    player_points = [0 for _ in range(players)]
 
-    for round_num in range(rounds):
-        while current_damage <= player_health:
-            keep_roll = input("Do you want to roll? yes/no:")
-            if keep_roll == "yes":
-                score = dice()
-                current_damage += score
-                print("Your damage is :" , current_damage)
-        
-            if keep_roll == "no":
-                print("Your turn is over")
-                print(f"You have done {current_damage} points of damage!")
-                break
+    while player1_health > 0 or player2_health > 0 :   
+        for turn_player in range(rounds):
+            current_damage = 0
+            while current_damage <= player_health:
+                keep_roll = input("Do you want to roll? yes/no:")
+                if keep_roll == "yes":
+                    score = dice()
+                    current_damage += score
+                    print("Your damage is :" , current_damage)
             
-            if score == 1:
-                print("You rolled a 1! Your turn is over!")
-                break
-        
-        round_num = 0
-        round_num += 1
+                if keep_roll == "no":
+                    print("Your turn is over")
+                    print(f"You have done {current_damage} points of damage!")
+                    break
+                    
+                if score == 1:
+                    print("You rolled a 1! Your turn is over!")
+                    current_damage = 0
+                    print(f"You have done {current_damage} points of damage!")
+                    break
+                
+            round_num = 0
+            round_num += 1
+            player_points[turn_player] += current_damage
 
+            print("your total score is :", player_points[turn_player])
 
+        player1_health = player_health - player_points[turn_player]
+        player2_health = player_health - player_points[turn_player]
+
+        if player1_health <= 0:
+            print("Winner is Player2!!")
+        if player2_health <= 0:
+            print("Winner is Player1!!")
+        print(player1_health)
+        print(player2_health)
 
 #
 
