afe51f89704aa75159e1a11c583bb47b8269a34d
EthanDesjardins <epd210000@utdallas.edu>
2023-12-01 21:56:38 -0600

    changed code

--- src/project.py
+++ src/project.py
@@ -7,6 +7,29 @@ def dice():
     
     return roll
 
+def round(damage):
+    current_damage = damage
+    keep_roll = input("Do you want to roll? yes/no:")
+    score = dice()
+    if keep_roll == "yes" and score != 1:
+        
+        current_damage += score
+        print("Your damage is :" , current_damage)
+        return current_damage
+    
+    if keep_roll == "no":
+        print("Your turn is over")
+        print(f"You have done {current_damage} points of damage!")
+        return current_damage
+        
+                    
+    if keep_roll == "yes" and score == 1:
+        print("You rolled a 1! Your turn is over!")
+        current_damage = 0
+        print(f"You have done {current_damage} points of damage!")
+        return current_damage
+        
+
 def main():
 
     while True:
@@ -18,55 +41,48 @@ def main():
                 break
         else:
             print("Invalid, please enetr a number between 1 and 10.")
-    
 
-
-#
     player_health = 20
     player1_health = player_health
     player2_health = player_health
     players = 2
     current_damage = 0
+    turn_counter = 0
     player_points = [0 for _ in range(players)]
 
     while player1_health > 0 or player2_health > 0 :   
         for turn_player in range(rounds):
             current_damage = 0
             while current_damage <= player_health:
-                keep_roll = input("Do you want to roll? yes/no:")
-                if keep_roll == "yes":
-                    score = dice()
-                    current_damage += score
-                    print("Your damage is :" , current_damage)
-            
-                if keep_roll == "no":
-                    print("Your turn is over")
-                    print(f"You have done {current_damage} points of damage!")
-                    break
-                    
-                if score == 1:
-                    print("You rolled a 1! Your turn is over!")
-                    current_damage = 0
-                    print(f"You have done {current_damage} points of damage!")
-                    break
                 
-            round_num = 0
-            round_num += 1
-            player_points[turn_player] += current_damage
+                current_damage = round(current_damage)
+                
+                
+                round_num = 0
+                round_num += 1
+                player_points[turn_player] += current_damage
 
-            print("your total score is :", player_points[turn_player])
+                print("your total score is :", player_points[turn_player])
+                turn_counter += 1
+                player1_health = player_health - player_points[turn_player]
+                player2_health = player_health - player_points[turn_player]
 
-        player1_health = player_health - player_points[turn_player]
-        player2_health = player_health - player_points[turn_player]
+            if turn_counter >= rounds:
+                break
 
-        if player1_health <= 0:
-            print("Winner is Player2!!")
-        if player2_health <= 0:
-            print("Winner is Player1!!")
-        print(player1_health)
-        print(player2_health)
+            if player1_health <= 0:
+                print("Winner is Player2!!")
+            if player2_health <= 0:
+                print("Winner is Player1!!")
 
-#
+    print(f"Player1 has {player1_health} health left, and Player2 has {player2_health} health left.")
+
+    if player1_health > player2_health:
+        print("Winner is Player1!!")
+    if player2_health > player1_health:
+        print("Winner is Player2!!")
+    if player1_health == player2_health:
+        print("It was a Tie!!")
 
 if __name__ == "__main__":
     main()
